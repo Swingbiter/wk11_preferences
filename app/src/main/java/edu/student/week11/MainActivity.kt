790d1b71dd96be2869c3a46e6fe5896b960f7fa0
Brady <35944413+Swingbiter@users.noreply.github.com>
2019-04-06 19:45:36 -0500

    Initial commit

---
+++ app/src/main/java/edu/student/week11/MainActivity.kt
@@ -0,0 +1,27 @@
+package edu.student.week11
+
+import android.content.Context
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val et1= findViewById<EditText>(R.id.txtEmail) as EditText
+        val preferences = getSharedPreferences("data", Context.MODE_PRIVATE)
+        et1.setText(preferences.getString("mail", ""))
+        val button1 = findViewById<Button>(R.id.btnSubmit) as Button
+        button1.setOnClickListener {
+            val editor = preferences.edit ()
+            editor.putString("mail", txtEmail.text.toString())
+            editor.commit()
+            finish()
+        }
+
+    }
+}
